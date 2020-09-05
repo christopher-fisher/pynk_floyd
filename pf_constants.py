f02520b275643eedf225dedcd99c02234e3b7b88
christopher-fisher <daeurth@gmail.com>
2020-09-04 21:55:30 -0400

    Made note of parameters that worked well

--- pf_constants.py
+++ pf_constants.py
@@ -4,11 +4,11 @@
 # TRAINING_DATA_PATH = "C:\\Users\\daeur\\PycharmProjects\\pynk_floyd\\Training Data\\darkside.txt"
 TRAINING_DATA_PATH = "C:\\Users\\daeur\\PycharmProjects\\pynk_floyd\\Training Data\\pinkfloyd.txt"
 
-EPOCHS = 15
+EPOCHS = 20
 
 # Lower = more predictable, higher = more surprising.
 # This is a good number to tweak
-TEMPERATURE = 1.15
+TEMPERATURE = 0.85
 
 # List of initial inputs for batch generation
 # This will change after the proof of concept stage
@@ -18,3 +18,11 @@ OUTPUT_SEEDS = ['P', 'I', 'N', 'K', 'F', 'L', 'O', 'Y', 'D']
 
 # Name to use in folder creation
 PROJECT_NAME = 'pinkfloyd'
+
+
+#####################################
+# Some settings that have worked well
+# EPOCHS 20 TEMPERATURE 0.85
+#
+#
+#
